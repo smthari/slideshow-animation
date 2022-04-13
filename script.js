f70a81045f27a4c72bddd173f530dd6cae870d64
Sumit Harijan <smtharijan@gmail.com>
2022-04-13 10:47:50 +0530

    slideshow gallery with prev next , change slide , active class

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+  /* console.log(slideIndex); */
+}
+
 function changeSlide(n) {
   showSlides((slideIndex += n));
-  // console.log(showSlides(slideIndex))
-  // console.log(slideIndex)
+  /* console.log(slideIndex); */
 }
 
 // we have to pass n as an parameter in function
 // we'll get the value of n by calling function and passing slideIndex as an argument
 function showSlides(n) {
+  // console.log(slideIndex);
+
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  console.log(typeof slides + " " + slides.length);
+  // console.log(typeof slides + " " + slides.length);
+  var dots = document.getElementsByClassName("change");
 
   // let's write condition to loop a slideshow
   // when n is greater then slides.length then value of slideIndex should be 1
@@ -30,6 +37,10 @@ function showSlides(n) {
     // console.log(slides[i])
   }
 
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
   // console.log(`slide index ${slideIndex - 1}`);
+  dots[slideIndex - 1].className += " active";
 }
